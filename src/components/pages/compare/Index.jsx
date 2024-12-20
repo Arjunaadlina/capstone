c00c032a545ddfd93da7d7e94808b2e878745e54
Arjunaadlina <arjunaadlinamartha@gmail.com>
2024-12-20 17:35:49 +0700

    add compare to local storage

--- src/components/pages/compare/Index.jsx
+++ src/components/pages/compare/Index.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCountryData } from "../../../redux/thunk";
 import Sugesstion from "./Sugesstion";
 import BarChart from "./BarChart";
 import PieChart from "./PieChart";
-import '../../../assets/css/skleton.css';
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCountryData } from "../../../redux/thunk";
-
+import "../../../assets/css/skleton.css";
 
 const Compare = () => {
   const dispatch = useDispatch();
@@ -14,41 +12,61 @@ const Compare = () => {
   const [searchTerm2, setSearchTerm2] = useState("");
   const [suggestions1, setSuggestions1] = useState([]);
   const [suggestions2, setSuggestions2] = useState([]);
-  const [selectedCountry1, setSelectedCountry1] = useState(null);
-  const [selectedCountry2, setSelectedCountry2] = useState(null);
+  const [selectedCountry1, setSelectedCountry1] = useState(
+    JSON.parse(localStorage.getItem("selectedCountry1")) || null
+  );
+  const [selectedCountry2, setSelectedCountry2] = useState(
+    JSON.parse(localStorage.getItem("selectedCountry2")) || null
+  );
   const loading = useSelector((state) => state.loading);
   const countries = useSelector((state) => state.sortedCountries);
 
   useEffect(() => {
-    if(countries.length === 0){
-        dispatch(fetchCountryData())
-        const timer = setTimeout(() => {
-          if (sortedCountries.length === 0) {
-              window.alert('Failed to fetch valid country data. Please refresh the page.');
-              window.location.reload();
-          }
+    if (countries.length === 0) {
+      dispatch(fetchCountryData());
+      const timer = setTimeout(() => {
+        if (countries.length === 0) {
+          window.alert(
+            "Failed to fetch valid country data. Please refresh the page."
+          );
+          window.location.reload();
+        }
       }, 10000);
 
       return () => clearTimeout(timer);
     }
-  }, [dispatch, countries.length])
+  }, [dispatch, countries.length]);
+
+  useEffect(() => {
+    localStorage.setItem(
+      "selectedCountry1",
+      JSON.stringify(selectedCountry1)
+    );
+  }, [selectedCountry1]);
+
+  useEffect(() => {
+    localStorage.setItem(
+      "selectedCountry2",
+      JSON.stringify(selectedCountry2)
+    );
+  }, [selectedCountry2]);
 
   if (loading) {
     return (
-        <div className='mt-20 sm:px-16 md:px-32 px-8'>
-            <div className="skeleton skeleton-hero"></div>
-            <div className="skeleton skeleton-content1"></div>
-            <div className="skeleton skeleton-content2"></div>
-            <div className="skeleton skeleton-content3"></div>
-        </div>
+      <div className="mt-20 sm:px-16 md:px-32 px-8">
+        <div className="skeleton skeleton-hero"></div>
+        <div className="skeleton skeleton-content1"></div>
+        <div className="skeleton skeleton-content2"></div>
+        <div className="skeleton skeleton-content3"></div>
+      </div>
     );
-}
+  }
 
   const handleSearch = (searchTerm, setSuggestions) => {
     const filteredCountries = countries.filter((c) =>
       c.name.common.toLowerCase().includes(searchTerm.toLowerCase())
     );
-    setSuggestions(filteredCountries.slice(0, 5)); 
+    setSuggestions(filteredCountries.slice(0, 5));
   };
 
   const handleSelect = (country, setSelectedCountry, setSearchTerm, setSuggestions) => {
@@ -57,36 +75,35 @@ const Compare = () => {
     setSuggestions([]);
   };
 
-
   return (
     <div className="p-6 space-y-6 lg:px-32">
       <div className="grid grid-cols-2 gap-4">
-        <Sugesstion 
-          selectedCountry={selectedCountry1} 
-          suggestions={suggestions1} 
-          searchTerm={searchTerm1} 
-          setSearchTerm={setSearchTerm1} 
-          setSuggestions={setSuggestions1} 
-          setSelectedCountry={setSelectedCountry1} 
-          handleSearch={handleSearch} 
-          handleSelect={handleSelect} 
+        <Sugesstion
+          selectedCountry={selectedCountry1}
+          suggestions={suggestions1}
+          searchTerm={searchTerm1}
+          setSearchTerm={setSearchTerm1}
+          setSuggestions={setSuggestions1}
+          setSelectedCountry={setSelectedCountry1}
+          handleSearch={handleSearch}
+          handleSelect={handleSelect}
         />
 
-        <Sugesstion 
-          selectedCountry={selectedCountry2} 
-          suggestions={suggestions2} 
-          searchTerm={searchTerm2} 
-          setSearchTerm={setSearchTerm2} 
-          setSuggestions={setSuggestions2} 
-          setSelectedCountry={setSelectedCountry2} 
-          handleSearch={handleSearch} 
-          handleSelect={handleSelect} 
+        <Sugesstion
+          selectedCountry={selectedCountry2}
+          suggestions={suggestions2}
+          searchTerm={searchTerm2}
+          setSearchTerm={setSearchTerm2}
+          setSuggestions={setSuggestions2}
+          setSelectedCountry={setSelectedCountry2}
+          handleSearch={handleSearch}
+          handleSelect={handleSelect}
         />
       </div>
 
       <div className="flex flex-col xl:flex-row w-full justify-between ">
-        <BarChart selectedCountry1={selectedCountry1} selectedCountry2={selectedCountry2}/>
-        <PieChart selectedCountry1={selectedCountry1} selectedCountry2={selectedCountry2}/>
+        <BarChart selectedCountry1={selectedCountry1} selectedCountry2={selectedCountry2} />
+        <PieChart selectedCountry1={selectedCountry1} selectedCountry2={selectedCountry2} />
       </div>
     </div>
   );
